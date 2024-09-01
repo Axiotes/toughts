76c54cba2e4d4b6ae7d7df20eb639e2c5c3beebc
Arthur Axiotes <axiotesarthur@gmail.com>
2024-09-01 18:15:02 -0300

    visibility password

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -11,4 +11,14 @@ import { RouterLink } from '@angular/router';
 export class RegisterComponent {
   public typeInput: string = 'password';
   public iconPassword: string = 'closed';
+
+  public toggleVisibility() {
+    if (this.typeInput === 'password') {
+      this.typeInput = 'text';
+      this.iconPassword = 'open';
+    } else {
+      this.typeInput = 'password';
+      this.iconPassword = 'closed';
+    }
+  }
 }
