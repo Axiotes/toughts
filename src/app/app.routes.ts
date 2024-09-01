d1477220ee22cabfa8fa451d5c94341bfdb45759
Arthur Axiotes <axiotesarthur@gmail.com>
2024-09-01 14:39:15 -0300

    feat: creation of routes and main components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ToughtsComponent } from './pages/toughts/toughts.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'toughts', component: ToughtsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/toughts', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutesModule {}
---
+++ src/app/pages/toughts/toughts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toughts',
+  standalone: true,
+  imports: [],
+  templateUrl: './toughts.component.html',
+  styleUrl: './toughts.component.scss'
+})
+export class ToughtsComponent {
+
+}
