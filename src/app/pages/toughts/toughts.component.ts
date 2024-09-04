d7106314f19a602af17420326b3400a424998f7d
Arthur Axiotes <axiotesarthur@gmail.com>
2024-09-03 23:38:28 -0300

    feat: layout of the thought component
    feat: receive thoughts from the database.

--- src/app/pages/toughts/toughts.component.ts
+++ src/app/pages/toughts/toughts.component.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { ThinkingComponent } from "../../components/thinking/thinking.component";
+import { ThinkingComponent } from '../../components/thinking/thinking.component';
+import { CommonModule } from '@angular/common';
+import { Tought } from '../../types/tought.type';
+import { ApiService } from '../../services/api.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-toughts',
   standalone: true,
-  imports: [MatIconModule, ThinkingComponent],
+  imports: [
+    MatIconModule,
+    ThinkingComponent,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './toughts.component.html',
-  styleUrl: './toughts.component.scss'
+  styleUrl: './toughts.component.scss',
 })
 export class ToughtsComponent {
+  public toughts: Tought[] = [];
+  public searchForm: FormGroup;
 
+  constructor(private apiService: ApiService) {
+    this.searchForm = new FormGroup({
+      search: new FormControl(''),
+    });
+
+    this.apiService.allToughts(this.search).subscribe({
+      next: (response) => {
+        response.forEach((res) => {
+          this.toughts.push(res);
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  public get search() {
+    return this.searchForm.get('search')?.value;
+  }
+
+  public searching() {
+    this.apiService.allToughts(this.search).subscribe({
+      next: (response) => {
+        this.toughts.length = 0;
+
+        response.forEach((res) => {
+          this.toughts.push(res);
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
