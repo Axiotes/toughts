134a9ad40eba628e77034f5db1472e8a09aa00d7
Arthur Axiotes <axiotesarthur@gmail.com>
2024-09-04 22:40:12 -0300

    feat: dashboard page layout

---
+++ src/app/components/my-tought/my-tought.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-tought',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-tought.component.html',
+  styleUrl: './my-tought.component.scss'
+})
+export class MyToughtComponent {
+
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MyToughtComponent } from "../../components/my-tought/my-tought.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MyToughtComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
